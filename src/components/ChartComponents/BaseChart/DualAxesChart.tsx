e77b72aea59d6e631d1ff383411c6efead8aa348
berk <berk@berkdeMacBook-Pro.local>
2025-09-17 15:34:08 +0800

    feat: 双轴图调整

--- src/components/ChartComponents/BaseChart/DualAxesChart.tsx
+++ src/components/ChartComponents/BaseChart/DualAxesChart.tsx
@@ -46,13 +46,15 @@ const DualAxesChart = forwardRef<any, ChartProps>((props, ref) => {
     ],
     legend: {
       color: {
-        // position: 'left-top',
         layout: {
           justifyContent: 'center',
         },
+        maxItemWidth: null,
+        itemSpacing: 8,
+        page: false,
         itemMarker: (field: string) => {
           if (field === leftYField) return 'rect';
-          return 'line';
+          return 'smooth';
         },
         itemLabelText: (field: { id: string }) => {
           if (field.id === leftYField) return leftAlias;
@@ -61,19 +63,30 @@ const DualAxesChart = forwardRef<any, ChartProps>((props, ref) => {
         },
       },
     },
-    tooltip: {
-      items: [
-        {
-          field: leftYField,
-          name: leftAlias,
-          valueFormatter: leftFormatter,
+    interaction: {
+      tooltip: {
+        // render 回调方法返回一个innerHTML 或者 DOM
+        render: (_event: any, { title, items }: any) => {
+          const leftItem = items.find((item: any) => item.name === leftYField);
+          const rightItem = items.find(
+            (item: any) => item.name === rightYField,
+          );
+          return `<div style="padding: 0.5rem"><div>${title}</div>
+            <div style="margin-top: 0.5rem; display: flex; align-items: center;">
+              <span style="display:inline-block;width:8px;height:8px;background-color:${
+                leftItem?.color
+              };border-radius:50%;margin-right:0.5rem;"></span>
+              <span>${leftAlias}: ${leftFormatter(leftItem?.value)}</span>
+            </div>
+            <div style="margin-top: 0.5rem; display: flex; align-items: center;">
+              <span style="display:inline-block;width:8px;height:8px;background-color:${
+                rightItem?.color
+              };border-radius:50%;margin-right:0.5rem;"></span>
+              <span>${rightAlias}: ${rightFormatter(rightItem?.value)}</span>
+            </div>
+          </div>`;
         },
-        {
-          field: rightYField,
-          name: rightAlias,
-          valueFormatter: rightFormatter,
-        },
-      ],
+      },
     },
   };
 
